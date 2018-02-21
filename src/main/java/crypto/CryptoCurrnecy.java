e76652b5e66b83cd8b4687446bb7cb68baeed5d0
ragnarockie08 <ragnarockie08@gmail.com>
2018-02-21 11:19:53 +0100

    read objects from json list

--- src/main/java/crypto/CryptoCurrnecy.java
+++ src/main/java/crypto/CryptoCurrnecy.java
@@ -9,10 +9,32 @@ import java.io.Serializable;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CryptoCurrnecy implements Serializable {
 
-    @JsonProperty("id")
     private String id;
-    @JsonProperty("name")
-    private double name;
+    private String name;
+    private String symbol;
+    private int rank;
+    @JsonProperty("price_usd")
+    private double priceUsd;
+    @JsonProperty("price_btc")
+    private double priceBtc;
+    @JsonProperty("24h_volume_usd")
+    private double VolumeUsd24;
+    @JsonProperty("market_cap_usd")
+    private double marketCapUsd;
+    @JsonProperty("available_supply")
+    private double availableSupply;
+    @JsonProperty("total_supply")
+    private double totalSupply;
+    @JsonProperty("max_supply")
+    private double maxSupply;
+    @JsonProperty("percent_change_1h")
+    private double percent1;
+    @JsonProperty("percent_change_24h")
+    private double percent24;
+    @JsonProperty("percent_change_7d")
+    private double percent7d;
+    @JsonProperty("last_updated")
+    private long lastUpdate;
 
     public CryptoCurrnecy(){
 
@@ -25,6 +47,63 @@ public class CryptoCurrnecy implements Serializable {
                 '}';
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public double getPriceUsd() {
+        return priceUsd;
+    }
+
+    public double getPriceBtc() {
+        return priceBtc;
+    }
+
+    public double getVolumeUsd24() {
+        return VolumeUsd24;
+    }
+
+    public double getMarketCapUsd() {
+        return marketCapUsd;
+    }
+
+    public double getAvailableSupply() {
+        return availableSupply;
+    }
+
+    public double getTotalSupply() {
+        return totalSupply;
+    }
+
+    public double getMaxSupply() {
+        return maxSupply;
+    }
+
+    public double getPercent1() {
+        return percent1;
+    }
+
+    public double getPercent24() {
+        return percent24;
+    }
+
+    public double getPercent7d() {
+        return percent7d;
+    }
+
+    public long getLastUpdate() {
+        return lastUpdate;
+    }
 }
